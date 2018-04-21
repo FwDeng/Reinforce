274e0283fa4c7f77793ff652e4473f436b8b4357
FwDeng <deng_fw@163.com>
2018-04-21 19:42:16 +0800

    Add DQN

---
+++ DQN/dqn.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*-
+import sys
+import random
+import gym
+import numpy as np
+from collections import deque
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+
+EPISODES = 1000
+
+
+# Deep Q-learning Agent
+class DQNAgent:
+    def __init__(self, state_size, action_size):
+        self.render = True
+        self.load_model = False
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = deque(maxlen=2000)    # deque是双端队列，其元素可以从两端弹出
+        self.gamma = 0.99    # discount rate
+        self.epsilon = 1.0  # exploration rate
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.999  # epsilon的衰减率
+        self.learning_rate = 0.001  # 神经网络的学习率
+        self.batch_size = 64
+        self.train_start = 1000
+        self.model = self.build_model()
+        self.target_model = self.build_model()
+        self.update_target_model()
+        if self.load_model:
+            self.model.load_weights("./save/cartpole-dqn.h5")
+
+    def build_model(self):
+        """
+        使用Keras建立神经网络，输入为状态，输出为动作
+        """
+        model = Sequential()
+        model.add(Dense(24, input_dim=self.state_size, activation='relu',
+                        kernel_initializer='he_uniform'))
+        model.add(Dense(24, activation='relu',
+                        kernel_initializer='he_uniform'))
+        model.add(Dense(self.action_size, activation='linear',
+                        kernel_initializer='he_uniform'))
+        model.summary()
+        model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
+        return model
+
+    def update_target_model(self):
+        self.target_model.set_weights(self.model.get_weights())
+
+    def remember(self, state, action, reward, next_state, done):
+        """
+        将序列添加到Replay Memory。注意memory是双端队列。
+        """
+        self.memory.append((state, action, reward, next_state, done))
+
+    def get_action(self, state):
+        """
+        采用epsilon-greedy策略
+        """
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        else:
+            q_value = self.model.predict(state)
+            return np.argmax(q_value[0])
+
+    def append_sample(self, state, action, reward, next_state, done):
+        """
+        将样本添加进Replay Memory
+        """
+        self.memory.append((state, action, reward, next_state, done))
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+    def train_model(self):
+        """
+        从Memory中随机取样本，并进行训练
+        """
+        if len(self.memory) < self.train_start:
+            return
+        batch_size = min(self.batch_size, len(self.memory))
+        mini_batch = random.sample(self.memory, batch_size)
+        update_input = np.zeros((batch_size, self.state_size))
+        update_target = np.zeros((batch_size, self.state_size))
+        action, reward, done = [], [], []
+
+        for i in range(self.batch_size):
+            update_input[i] = mini_batch[i][0]
+            action.append(mini_batch[i][1])
+            reward.append(mini_batch[i][2])
+            update_target[i] = mini_batch[i][3]
+            done.append(mini_batch[i][4])
+
+        target = self.model.predict(update_input)
+        target_val = self.target_model.predict(update_target)
+
+        for i in range(self.batch_size):
+            if done[i]:
+                target[i][action[i]] = reward[i]
+            else:
+                target[i][action[i]] = reward[i] + self.gamma * (
+                    np.amax(target_val[i]))
+
+        self.model.fit(update_input, target, batch_size=self.batch_size,
+                       epochs=1, verbose=0)
+
+
+if __name__ == "__main__":
+    env = gym.make('CartPole-v1')
+    env._max_episode_steps = None
+    state_size = env.observation_space.shape[0]
+    action_size = env.action_space.n
+    agent = DQNAgent(state_size, action_size)
+    scores, episodes = [], []
+
+    for e in range(EPISODES):
+        done = False
+        score = 0
+        state = env.reset()
+        state = np.reshape(state, [1, state_size])
+
+        while not done:
+            if agent.render:
+                env.render()
+
+            # get action for the current state and go one step in environment
+            action = agent.get_action(state)
+            next_state, reward, done, info = env.step(action)
+            next_state = np.reshape(next_state, [1, state_size])
+            # if an action make the episode end, then gives penalty of -100
+            reward = reward if not done or score == 499 else -100
+
+            # save the sample <s, a, r, s'> to the replay memory
+            agent.append_sample(state, action, reward, next_state, done)
+            # every time step do the training
+            agent.train_model()
+            score += reward
+            state = next_state
+
+            if done:
+                # every episode update the target model to be same with model
+                agent.update_target_model()
+
+                # every episode, plot the play time
+                score = score if score == 500 else score + 100
+                scores.append(score)
+                episodes.append(e)
+                print("episode:", e, "  score:", score, "  memory length:",
+                      len(agent.memory), "  epsilon:", agent.epsilon)
+
+                # if the mean of scores of last 10 episode is bigger than 490
+                # stop training
+                if np.mean(scores[-min(10, len(scores)):]) > 490:
+                    sys.exit()
+
+        # save the model
+        if e % 50 == 0:
+            agent.model.save_weights("./save/cartpole_dqn.h5")
